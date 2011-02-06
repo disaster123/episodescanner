d74997d61aef930b6efeda5fc7fcf52318257d08
spriebe <spriebe@5246ad79-0940-4c30-aa19-b15fe6fa35ba>
2011-02-06 23:00:19 +0000

    - use filename without .pl ending while compiling with pp
    git-svn-id: https://svn.de-nserver.de/svn/spriebe/MP/episodescanner@216 5246ad79-0940-4c30-aa19-b15fe6fa35ba

--- build/build-pp.bat
+++ build/build-pp.bat
@@ -9,14 +9,14 @@ if not "%ProgramFiles(x86)%".=="". set progpath=%ProgramFiles(x86)%
 
 echo ".svn" >No.SVN.txt
 
-xcopy ..\episodescanner.pl build
+xcopy ..\episodescanner.pl build\episodescanner
 xcopy ..\libs build\libs /EXCLUDE:No.SVN.txt /A /S /I /E /Q /Y
 
 cd build
 REM -c is not possible with Crypto
 REM -C  DO clean Cache libs and files
 REM Archive::Unzip::Burst
-cmd /C pp -v  -I libs -f PodStrip -F PodStrip -M PAR::Filter::PodStrip -f Crypto -F Crypto -M Filter::Crypto::Decrypt -X Carp -X DBD::SQLite -X DBD::CSV -X DBD::File -X DBD::Excel -o episodescanner.exe episodescanner.pl
+cmd /C pp -v  -I libs -f PodStrip -F PodStrip -M PAR::Filter::PodStrip -f Crypto -F Crypto -M Filter::Crypto::Decrypt -X Carp -X DBD::SQLite -X DBD::CSV -X DBD::File -X DBD::Excel -o episodescanner.exe episodescanner
 cd ..
 
 xcopy ..\*.dll build
@@ -29,6 +29,7 @@ xcopy ..\tmp build\tmp /EXCLUDE:No.SVN.txt /A /S /I /E /Q /Y
 rmdir /S /Q build\libs
 del /S /Q build\tmp\*
 del /S /Q build\*.pl
+del /S /Q build\episodescanner
 del No.SVN.txt
 
 move build episodescanner
